be5625165b2cf1497c695b3f26f8c14dcc0c9976
ami patel <amipatel081103@gmail.com>
2024-06-21 15:07:04 +0530

    commit

---
+++ File1.js
@@ -0,0 +1,28 @@
+console.log('Hello I am Ami Patel')
+
+
+let a=10;
+let b=20;
+
+console.log("Value of a is:"+a);
+console.log("Addition:"+(a+b));
+
+for (let i=0; i<5; i++)
+    {
+        console.log(i);
+    }
+
+    const fruits=[];
+    fruits[0]="Apple";
+    fruits[1]="Banana";
+    fruits[2]="Orange";
+    fruits[3]="Mango";
+    fruits[4]="Grapes";
+    console.log(fruits);
+
+    
+    let x=8;
+    while(x<8){
+        console.log(x);
+        x++;
+    }
